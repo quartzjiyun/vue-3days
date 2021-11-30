a1d79709983e286b0ffda9dd5407b5f6ec1a222a
Seungwon Go <seungwon.go@gmail.com>
2021-11-30 10:43:14 +0900

    update

---
+++ project02/src/components/SimpleGrid.vue
@@ -0,0 +1,97 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th v-if="selectType !== ''"></th>
+        <th :key="i" v-for="(h, i) in headers">{{ h.title }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr :key="i" v-for="(item, i) in items">
+        <td v-if="selectType === 'radio'">
+          <input
+            type="radio"
+            :value="item[checkedKey]"
+            v-model="checkedItem"
+            @change="doChange"
+          />
+        </td>
+        <td v-else-if="selectType === 'checkbox'">
+          <input
+            type="checkbox"
+            :value="item[checkedKey]"
+            v-model="checkedItems"
+            @change="doChange"
+          />
+        </td>
+        <td :key="j" v-for="(h, j) in headers">{{ item[h['key']] }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  name: '',
+  components: {},
+  props: {
+    headers: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    items: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    selectType: {
+      type: String,
+      default: ''
+    },
+    checkedKey: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      checkedItem: '',
+      checkedItems: []
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    doChange() {
+      if (this.selectType === 'radio') {
+        this.$emit('change-item', this.checkedItem)
+        // console.log(this.checkedItem)
+      } else if (this.selectType === 'checkbox') {
+        this.$emit('change-item', this.checkedItems)
+        // console.log(this.checkedItems)
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+table {
+  width: 100%;
+}
+
+table,
+th,
+td {
+  border-collapse: collapse;
+}
+
+th,
+td {
+  border: 1px solid #222;
+  padding: 10px;
+}
+</style>
--- project02/src/views/4_reuse/ReuseComponent2.vue
+++ project02/src/views/4_reuse/ReuseComponent2.vue
@@ -1,16 +1,35 @@
 <template>
   <div>
     <button @click="getUserList">조회</button>
+    <button @click="doDelete">삭제</button>
+    <simple-grid
+      :headers="headers"
+      :items="userList"
+      selectType="radio"
+      checkedKey="id"
+      @change-item="doChange"
+    />
   </div>
 </template>
 <script>
 import axios from 'axios'
+import SimpleGrid from '@/components/SimpleGrid.vue'
 export default {
   name: '',
-  components: {},
+  components: { 'simple-grid': SimpleGrid },
   data() {
     return {
-      userList: []
+      headers: [
+        { title: 'Name', key: 'name' },
+        { title: 'Email', key: 'email' },
+        { title: 'Phone', key: 'phone' },
+        { title: 'Company', key: 'company' },
+        // { title: 'Gender', key: 'gender' },
+        { title: 'Address', key: 'address' }
+      ],
+      userList: [],
+      chekcedItem: '',
+      chekcedItems: []
     }
   },
   setup() {},
@@ -24,7 +43,31 @@ export default {
       this.userList = (await axios.get('http://localhost:3000/users')).data
       // name, email, phone, company, address
       console.log(this.userList)
+    },
+    doChange(data) {
+      this.chekcedItem = data
+      console.log(data)
+    },
+    doDelete() {
+      // this.chekcedItem
     }
   }
 }
 </script>
+<style scoped>
+table {
+  width: 100%;
+}
+
+table,
+th,
+td {
+  border-collapse: collapse;
+}
+
+th,
+td {
+  border: 1px solid #222;
+  padding: 10px;
+}
+</style>
